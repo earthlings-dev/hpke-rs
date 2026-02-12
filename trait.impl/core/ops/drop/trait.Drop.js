ae8c7af41025c4d0e5170a552ba702dff0b5ea2a
franziskuskiefer <franziskuskiefer@users.noreply.github.com>
2026-02-12 11:37:29 +0000

    deploy: b54c8bb83906331bdf4f606cafa30cd7fd20b531

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hpke_rs",[["impl&lt;Crypto: 'static + HpkeCrypto&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"hpke_rs/struct.Context.html\" title=\"struct hpke_rs::Context\">Context</a>&lt;Crypto&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[317]}
--- trait.impl/zeroize/trait.Zeroize.js
+++ trait.impl/zeroize/trait.Zeroize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hpke_rs",[["impl Zeroize for <a class=\"struct\" href=\"hpke_rs/struct.HpkePrivateKey.html\" title=\"struct hpke_rs::HpkePrivateKey\">HpkePrivateKey</a>"]]]]);
+    var implementors = Object.fromEntries([["hpke_rs",[["impl Zeroize for <a class=\"enum\" href=\"hpke_rs/enum.Mode.html\" title=\"enum hpke_rs::Mode\">Mode</a>"],["impl Zeroize for <a class=\"struct\" href=\"hpke_rs/struct.HpkePrivateKey.html\" title=\"struct hpke_rs::HpkePrivateKey\">HpkePrivateKey</a>"],["impl&lt;Crypto: 'static + HpkeCrypto&gt; Zeroize for <a class=\"struct\" href=\"hpke_rs/struct.Context.html\" title=\"struct hpke_rs::Context\">Context</a>&lt;Crypto&gt;"],["impl&lt;Crypto: 'static + HpkeCrypto&gt; Zeroize for <a class=\"struct\" href=\"hpke_rs/struct.Hpke.html\" title=\"struct hpke_rs::Hpke\">Hpke</a>&lt;Crypto&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[158]}
+//{"start":57,"fragment_lengths":[606]}
